b57375461ffb98d4086a8c94fe21d91c6a53936a
MarcoCslt <marco.casalta@free.fr>
2018-10-18 10:45:09 +0200

    Index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Table Multiplication</title>
+</head>
+<body>
+    <?php
+        for ($i=1; $i < 4; $i++) { 
+            echo "<a href='table.php?number=".$i."'> Table de ".$i." </a><br>";
+        }
+    ?>
+</body>
+</html>
